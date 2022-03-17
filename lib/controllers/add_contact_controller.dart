a79afd83d3a73f220c762adc205bcbf6d7b645ce
Aziz Soulé <51265402+azizsoule@users.noreply.github.com>
2022-03-17 10:01:50 +0000

    implement add contact feature

--- lib/controllers/add_contact_controller.dart
+++ lib/controllers/add_contact_controller.dart
@@ -1,7 +1,44 @@
+import 'package:asna_chat/controllers/contact_controller.dart';
+import 'package:asna_chat/providers/firestore_provider.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:get/get.dart';
+
+import '../models/contact.dart';
 
 class AddContactController extends GetxController {
 
+  final FirestoreProvider _firestoreProvider = FirestoreProvider();
 
+  final TextEditingController nameController = TextEditingController();
+
+  String? contactStatus;
+
+  final TextEditingController contactController = TextEditingController();
+
+  void onContactStatusChanged(String? v) {
+    contactStatus = v;
+    update();
+  }
+
+  final TextEditingController specialityController = TextEditingController();
+
+  void onAddButtonPress() async {
+    Contact? c = await _firestoreProvider.addContact(Contact(
+      fullName: nameController.text.trim(),
+      contact: contactController.text.trim(),
+      personStatus: contactStatus,
+      speciality: specialityController.text.trim()
+    ));
+
+    if (c != null) {
+      Get.find<ContactController>().update();
+      contactController.clear();
+      nameController.clear();
+      specialityController.clear();
+      Get.back();
+    } else {
+      print("Erreur");
+    }
+  }
 
 }
--- lib/screens/add_contact_screen.dart
+++ lib/screens/add_contact_screen.dart
@@ -1,6 +1,9 @@
 import 'package:asna_chat/controllers/add_contact_controller.dart';
 import 'package:asna_chat/screens/base_view.dart';
+import 'package:asna_chat/widgets/app_button.dart';
 import 'package:flutter/material.dart';
+
+import '../constants/app_colors.dart';
 
 class AddContactScreen extends BaseView<AddContactController> {
 
@@ -8,7 +11,68 @@ class AddContactScreen extends BaseView<AddContactController> {
 
   @override
   Widget ui(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Ajouter un nouveau contact"),
+      ),
+      body: ListView(
+        padding: EdgeInsets.all(24),
+        children: [
+
+          TextFormField(
+            decoration: InputDecoration(
+              hintText: "Nom complet",
+            ),
+            controller: controller.nameController,
+          ),
+
+          SizedBox(height: 20,),
+
+          TextFormField(
+            decoration: InputDecoration(
+              hintText: "Contact",
+            ),
+            controller: controller.contactController,
+          ),
+
+          SizedBox(height: 20,),
+
+          DropdownButton(
+            hint: Text("Statut"),
+            value: controller.contactStatus,
+            items: [
+              DropdownMenuItem(child: Text("Professeur"), value: "P",),
+              DropdownMenuItem(child: Text("Etudiant"), value: "E",),
+            ],
+            onChanged: controller.onContactStatusChanged,
+          ),
+
+          SizedBox(height: 20,),
+
+          TextFormField(
+            decoration: InputDecoration(
+                hintText: "Spécialité / Filière"
+            ),
+            controller: controller.specialityController,
+          ),
+
+          SizedBox(height: 20,),
+
+          AppButton(
+            height: 50,
+            child: const Text("Ajouter", style: TextStyle(
+              color: Colors.white,
+            ),),
+            color: AppColors.green,
+            shapeBorder: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(100),
+            ),
+            onTap: controller.onAddButtonPress,
+          )
+
+        ],
+      ),
+    );
   }
 
 }
